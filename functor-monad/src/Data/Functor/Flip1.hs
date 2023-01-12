24865f27db7fdee1c22182b3f1ef3cca645b8e0b
Koji Miyazato <viercc@gmail.com>
2023-01-12 18:31:23 +0900

    Fix Show1, Read1 instance of (Flip1 t f g)
    They were incorrectly same to the Show1/Read1 instance of
    underlying (t g f), disagreeing stock derived Show/Read instance.

--- functor-monad/src/Data/Functor/Flip1.hs
+++ functor-monad/src/Data/Functor/Flip1.hs
@@ -3,14 +3,18 @@
 {-# LANGUAGE FlexibleInstances #-}
 {-# LANGUAGE MultiParamTypeClasses #-}
 {-# LANGUAGE PolyKinds #-}
+{-# LANGUAGE QuantifiedConstraints #-}
+{-# LANGUAGE StandaloneDeriving #-}
 {-# LANGUAGE StandaloneKindSignatures #-}
 {-# LANGUAGE UndecidableInstances #-}
 
 module Data.Functor.Flip1 where
 
+import AutoLift (Reflected1 (..))
 import Control.Applicative
 import Control.Monad
 import Control.Monad.Free (MonadFree (..))
+import Data.Coerce
 import Data.Functor.Classes
 import Data.Kind (Type)
 
@@ -27,7 +31,23 @@ newtype Flip1 t f g x = Flip1 {unFlip1 :: t g f x}
       MonadFail
     )
     via (t g f)
-  deriving (Eq1, Ord1, Show1, Read1) via (t g f)
+  deriving (Eq1, Ord1) via (t g f)
+
+deriving via
+  (Reflected1 (Flip1 t f g))
+  instance
+    ( forall a. Show a => Show (t g f a),
+      forall x y. Coercible x y => Coercible (t g f x) (t g f y)
+    ) =>
+    Show1 (Flip1 t f g)
+
+deriving via
+  (Reflected1 (Flip1 t f g))
+  instance
+    ( forall a. Read a => Read (t g f a),
+      forall x y. Coercible x y => Coercible (t g f x) (t g f y)
+    ) =>
+    Read1 (Flip1 t f g)
 
 instance (Functor h, MonadFree h (t g f)) => MonadFree h (Flip1 t f g) where
   wrap = Flip1 . wrap . fmap unFlip1
